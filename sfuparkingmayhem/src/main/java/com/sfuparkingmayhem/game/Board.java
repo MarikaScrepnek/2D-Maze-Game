2e210088c42d1464e8411a8598ef0a69df565b97
Marika Screpnek <mscrepnek2004@gmail.com>
2025-02-20 15:33:09 -0800

    Create Board.java that extends a Jpanel, sets its size, and sets its background color

---
+++ sfuparkingmayhem/src/main/java/com/sfuparkingmayhem/game/Board.java
@@ -0,0 +1,16 @@
+package com.sfuparkingmayhem.game;
+
+import java.awt.Color;
+import java.awt.Dimension;
+
+import javax.swing.JPanel;
+
+public class Board extends JPanel {
+
+    public Board() {
+        //set game board size to be the same as the window size
+        setPreferredSize(new Dimension(1280,720));
+        //set the background color to a concrete grey
+        setBackground(new Color(153, 153, 153));
+    }
+}
