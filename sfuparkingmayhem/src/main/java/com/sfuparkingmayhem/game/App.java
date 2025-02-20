72b8ad0d978077de8b9698f58845935cbf8daec9
Marika Screpnek <mscrepnek2004@gmail.com>
2025-02-19 16:34:03 -0800

    Changing dimensions to 1280x720, making sure the program shuts down when closing the window, and setting the window to be visible by the user

--- sfuparkingmayhem/src/main/java/com/sfuparkingmayhem/game/App.java
+++ sfuparkingmayhem/src/main/java/com/sfuparkingmayhem/game/App.java
@@ -1,9 +1,9 @@
 package com.sfuparkingmayhem.game;
 
-import javax.swing.*;
+import javax.swing.JFrame;
 
 /**
- * Hello world!
+ *
  *
  */
 public class App 
@@ -11,8 +11,12 @@ public class App
     public static void main( String[] args )
     {
         //create a window in which the game will run
-        Jframe window = new JFrame("Game Window");
-        //set the dimensions of the window to be compatible with both 1920x1080 and 1280x720 screens
-        window.setSize = (640,360);
+        JFrame window = new JFrame("Game Window");
+        //make sure the program shuts down when the window is closed
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //set the dimensions of the window to be 1280x720 (standard)
+        window.setSize(1280,720);
+        //make the window visible to the user
+        window.setVisible(true);
     }
 }
