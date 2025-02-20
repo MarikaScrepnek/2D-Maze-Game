a626726cf0f256eed0470362fb4cbc2a6979c1bf
Marika Screpnek <mscrepnek2004@gmail.com>
2025-02-19 16:18:22 -0800

    Creating window for the game and setting the window size

--- sfuparkingmayhem/src/main/java/com/sfuparkingmayhem/game/App.java
+++ sfuparkingmayhem/src/main/java/com/sfuparkingmayhem/game/App.java
@@ -1,5 +1,7 @@
 package com.sfuparkingmayhem.game;
 
+import javax.swing.*;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,9 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        //create a window in which the game will run
+        Jframe window = new JFrame("Game Window");
+        //set the dimensions of the window to be compatible with both 1920x1080 and 1280x720 screens
+        window.setSize = (640,360);
     }
 }
