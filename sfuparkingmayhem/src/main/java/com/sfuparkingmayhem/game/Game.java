60f2a9230ff82a7b104e339404471e2dfbff021f
Marika Screpnek <mscrepnek2004@gmail.com>
2025-02-20 15:14:28 -0800

    Change window name

--- sfuparkingmayhem/src/main/java/com/sfuparkingmayhem/game/Game.java
+++ sfuparkingmayhem/src/main/java/com/sfuparkingmayhem/game/Game.java
@@ -12,7 +12,7 @@ public class Game
     public void initWindow()
     {
         //create a window in which the game will run
-        JFrame window = new JFrame("Game Window");
+        JFrame window = new JFrame("SFU Parking Mayhem");
         //make sure the program shuts down when the window is closed
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         //set the dimensions of the window to be 1280x720 (standard)
