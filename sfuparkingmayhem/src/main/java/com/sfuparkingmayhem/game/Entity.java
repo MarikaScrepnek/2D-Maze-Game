880b419f01434f9805ec3b9e6f65aad46d6876c3
ka-lu <kml43@sfu.ca>
2025-02-20 12:03:37 -0800

    Added attributes to Entity class

--- sfuparkingmayhem/src/main/java/com/sfuparkingmayhem/game/Entity.java
+++ sfuparkingmayhem/src/main/java/com/sfuparkingmayhem/game/Entity.java
@@ -1,4 +1,12 @@
 package com.sfuparkingmayhem.game;
 
-public class Entity {
+//should this class be abstract since no "Entity" objects will be instantiated (always creating
+//objects like MainCharacter, Concord officer, coins etc.)
+public abstract class Entity {
+
+    //the attributes of an entity on the game board
+    protected int x_coordinate;
+    protected int y_coordinate;
+
+
 }
