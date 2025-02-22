c64595a6f77e21395805caf6498dd9bbfee087e1
ka-lu <kml43@sfu.ca>
2025-02-21 20:57:24 -0800

    added MainCharacter constructor

--- sfuparkingmayhem/src/main/java/com/sfuparkingmayhem/game/MainCharacter.java
+++ sfuparkingmayhem/src/main/java/com/sfuparkingmayhem/game/MainCharacter.java
@@ -1,4 +1,10 @@
 package com.sfuparkingmayhem.game;
 
-public class MainCharacter {
+public class MainCharacter extends MovingEntity {
+
+    //constructor for the MainCharcter
+    MainCharacter() {
+        super();
+    }
+
 }
