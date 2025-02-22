0a1cc8b50b9462cddd3adac316a3109117035010
ka-lu <kml43@sfu.ca>
2025-02-21 21:52:10 -0800

    added setImage() to MainCharacter class

--- sfuparkingmayhem/src/main/java/com/sfuparkingmayhem/game/MainCharacter.java
+++ sfuparkingmayhem/src/main/java/com/sfuparkingmayhem/game/MainCharacter.java
@@ -1,9 +1,25 @@
 package com.sfuparkingmayhem.game;
 
+import javax.imageio.ImageIO;
+import java.io.File;
+import java.io.IOException;
+
 public abstract class MainCharacter extends MovingEntity {
 
     //constructor for the MainCharcter
     MainCharacter(int x_coordinate, int y_coordinate) {
         super( x_coordinate, y_coordinate);
+        setImage();
+    }
+
+    //sets the image for the moving entity
+    private void setImage (){
+        try{
+            this.theImage= ImageIO.read(new File("resources/blue_square.png"));
+        }
+        catch(IOException e){
+            System.out.println(e.getMessage());
+        }
+
     }
 }
