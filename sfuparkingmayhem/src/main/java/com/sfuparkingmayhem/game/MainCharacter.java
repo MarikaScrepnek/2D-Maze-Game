ea97fa364a09e97f3ddfefdf35e5f9782e3aafaf
ka-lu <kml43@sfu.ca>
2025-02-24 13:12:14 -0800

    replaced setImage() wtih getImage() in MainChar class

--- sfuparkingmayhem/src/main/java/com/sfuparkingmayhem/game/MainCharacter.java
+++ sfuparkingmayhem/src/main/java/com/sfuparkingmayhem/game/MainCharacter.java
@@ -16,11 +16,23 @@ public abstract class MainCharacter extends MovingEntity {
     //constructor for the MainCharcter
     MainCharacter(int x_coordinate, int y_coordinate) {
         super( x_coordinate, y_coordinate);
-        setImage();
+        //setImage();
+        getImage();
     }
 
     //sets the image for the moving entity
-    private void setImage (){
+//    private void setImage (){
+//        try{
+//            this.theImage= ImageIO.read(new File("resources/blue_square.png"));
+//        }
+//        catch(IOException e){
+//            System.out.println(e.getMessage());
+//        }
+//    }
+
+
+
+    protected void getImage(){
         try{
             this.theImage= ImageIO.read(new File("resources/blue_square.png"));
         }
