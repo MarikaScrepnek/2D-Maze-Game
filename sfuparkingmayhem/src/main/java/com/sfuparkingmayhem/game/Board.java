fa4e439272fbbdd46f30e13ea37030f89d03b1f0
ka-lu <kml43@sfu.ca>
2025-02-24 12:33:38 -0800

    commit

--- sfuparkingmayhem/src/main/java/com/sfuparkingmayhem/game/Board.java
+++ sfuparkingmayhem/src/main/java/com/sfuparkingmayhem/game/Board.java
@@ -7,18 +7,10 @@ import javax.swing.JPanel;
 
 public class Board extends JPanel{
 
-    private final int DELAY = 25;
-
-    private MainCharacter player;
-    //private Timer timer;
-
     public Board() {
         //set game board size to be the same as the window size
         setPreferredSize(new Dimension(1280,720));
         //set the background color to a concrete grey
         setBackground(new Color(153, 153, 153));
-
-        // initialize game state
-        
     }
 }
