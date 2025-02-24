a93442c66ef795b458d1f28d1bea16931dd28f50
Marika Screpnek <mscrepnek2004@gmail.com>
2025-02-24 11:49:01 -0800

    Add rows, columns, cellsize, and board to board class

--- sfuparkingmayhem/src/main/java/com/sfuparkingmayhem/game/Board.java
+++ sfuparkingmayhem/src/main/java/com/sfuparkingmayhem/game/Board.java
@@ -6,7 +6,10 @@ import java.awt.Dimension;
 import javax.swing.JPanel;
 
 public class Board extends JPanel{
-
+    private final int rows = 15;
+    private final int columns = 15;
+    private final int cellsize = 50;
+    private final int[][] board = new int[rows][columns];
     public Board() {
         //set game board size to be the same as the window size
         setPreferredSize(new Dimension(1280,720));
