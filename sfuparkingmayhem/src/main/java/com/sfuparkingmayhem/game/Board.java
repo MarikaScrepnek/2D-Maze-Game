c6cf0e66735e6ecb9740250ca78f053ee295728a
Matthew Pham <mpa12@sfu.ca>
2025-02-24 16:05:10 -0800

    Add actionPerformed method in Board.java

--- sfuparkingmayhem/src/main/java/com/sfuparkingmayhem/game/Board.java
+++ sfuparkingmayhem/src/main/java/com/sfuparkingmayhem/game/Board.java
@@ -3,12 +3,14 @@ package com.sfuparkingmayhem.game;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 import javax.swing.JPanel;
 
-public class Board extends JPanel implements KeyListener{
+public class Board extends JPanel implements ActionListener, KeyListener{
     protected static final int ROWS = 15;
     protected static final int COLUMNS = 15;
     protected static final int CELL_SIZE = 50;
@@ -29,6 +31,15 @@ public class Board extends JPanel implements KeyListener{
         setBackground(new Color(153, 153, 153));
         //intialize player
         main_character = new MainCharacter(0, 1);
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // This method is to update the state of the game
+
+        // prevent the player from disapearing off the board
+        main_character.tickCharacter();
+
+        repaint();
     }
 
     @Override
