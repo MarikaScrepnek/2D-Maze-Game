f70172012f28667a3cf5a063e28ea458617cc127
Ryan Fung <rjf9@sfu.ca>
2025-02-25 21:44:08 -0800

    Create constructor

--- sfuparkingmayhem/src/main/java/com/sfuparkingmayhem/game/ConcordOfficer.java
+++ sfuparkingmayhem/src/main/java/com/sfuparkingmayhem/game/ConcordOfficer.java
@@ -1,5 +1,20 @@
 package com.sfuparkingmayhem.game;
 
-public class ConcordOfficer {
+public class ConcordOfficer extends MovingEntity{
+    // Reference to the MainCharacter
+    private MainCharacter target;
+
+    /**
+     * Constructs a ConcordOfficer that will chase the MainCharacter.
+     *
+     * @param x_coordinate The initial x-coordinate of this ConcordOfficer.
+     * @param y_coordinate The initial y-coordinate of this ConcordOfficer.
+     * @param target       The MainCharacter to chase.
+     */
+    public ConcordOfficer(int x_coordinate, int y_coordinate, MainCharacter target) {
+        super(x_coordinate, y_coordinate);
+        this.target = target;
+        getImage(); // Load the sprite/image for this entity
+    }
     
 }
