5743d1879075b663a32bbdcc55a4be0f54bc7a49
Ryan Fung <rjf9@sfu.ca>
2025-02-25 21:23:37 -0800

    Create abstract move method

--- sfuparkingmayhem/src/main/java/com/sfuparkingmayhem/game/MovingEntity.java
+++ sfuparkingmayhem/src/main/java/com/sfuparkingmayhem/game/MovingEntity.java
@@ -1,7 +1,16 @@
 package com.sfuparkingmayhem.game;
 
 public abstract class MovingEntity extends Entity {
-    MovingEntity(int x_coordinate, int y_coordinate) {
+    /**
+     * Constructs a MovingEntity with the specified coordinates.
+     *
+     * @param x_coordinate The initial x-coordinate.
+     * @param y_coordinate The initial y-coordinate.
+     */
+    public MovingEntity(int x_coordinate, int y_coordinate) {
         super(x_coordinate, y_coordinate);
     }
+
+    protected abstract void move();
+
 }
