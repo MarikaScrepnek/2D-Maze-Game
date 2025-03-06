59ee09f502722a62da2ac074cbe665f72c5c856d
ka-lu <kml43@sfu.ca>
2025-03-05 22:33:03 -0800

    added isCollected attribute to Reward class

--- sfuparkingmayhem/src/main/java/com/sfuparkingmayhem/game/Reward.java
+++ sfuparkingmayhem/src/main/java/com/sfuparkingmayhem/game/Reward.java
@@ -1,6 +1,10 @@
 package com.sfuparkingmayhem.game;
 
 public abstract class Reward extends StaticEntity {
+
+    private boolean isCollected = false;
+
+
     Reward(int x, int y) {
         super(x, y);
     }
