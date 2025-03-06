bc8c5153955988f4549e2ed1cd0fa08bee6c5218
ka-lu <kml43@sfu.ca>
2025-03-05 22:31:41 -0800

    added expiryTime to LostNote class

--- sfuparkingmayhem/src/main/java/com/sfuparkingmayhem/game/LostNote.java
+++ sfuparkingmayhem/src/main/java/com/sfuparkingmayhem/game/LostNote.java
@@ -3,6 +3,7 @@ package com.sfuparkingmayhem.game;
 public class LostNote extends Reward {
 
     private int value = 10;
+    private int expiryTime = 5; //put 5 seconds as the time limit
 
     LostNote(int x, int y) {
         super(x, y);
