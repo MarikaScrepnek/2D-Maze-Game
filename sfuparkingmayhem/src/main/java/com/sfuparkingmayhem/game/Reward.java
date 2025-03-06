c5aa373c31ac2be556d223f040d5ae0a952aec25
ka-lu <kml43@sfu.ca>
2025-03-05 22:34:49 -0800

    added value attribute to Reward class

--- sfuparkingmayhem/src/main/java/com/sfuparkingmayhem/game/Reward.java
+++ sfuparkingmayhem/src/main/java/com/sfuparkingmayhem/game/Reward.java
@@ -3,6 +3,7 @@ package com.sfuparkingmayhem.game;
 public abstract class Reward extends StaticEntity {
 
     private boolean isCollected = false;
+    private int value;
 
 
     Reward(int x, int y) {
