75f320e1ef9438d8a3e95c33375b444a99b0e2c3
Marika Screpnek <mscrepnek2004@gmail.com>
2025-03-09 12:39:58 -0700

    Add comments to MainMenu.java, add draw method in MainMenu.java

--- sfuparkingmayhem/src/main/java/com/sfuparkingmayhem/screens/MainMenu.java
+++ sfuparkingmayhem/src/main/java/com/sfuparkingmayhem/screens/MainMenu.java
@@ -1,23 +1,24 @@
 package com.sfuparkingmayhem.screens;
 
+import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
 
 public class MainMenu {
-    private int x,y;
-    private BufferedImage start_game;
-    private BufferedImage instructions;
-    private BufferedImage quit;
+    private int x,y; //mouse position
+    private BufferedImage start_game; //start game button image
+    private BufferedImage instructions; //instructions button image
+    private BufferedImage quit; //quit button image
 
-    public MainMenu(int x, int y) {
+    public MainMenu(int x, int y) { //initializes variables and load button images
         this.x=x;
         this.y=y;
         load_images();
     }
 
-    private void load_images() {
+    private void load_images() { //method to load button images
         try{
             this.start_game = ImageIO.read(getClass().getClassLoader().getResourceAsStream("start_game.png"));
         }
@@ -36,5 +37,9 @@ public class MainMenu {
         catch(IOException e){
             System.out.println("Error loading main" + e.getMessage());
         }
+    }
+
+    public void draw (Graphics g) {
+        
     }
 }
