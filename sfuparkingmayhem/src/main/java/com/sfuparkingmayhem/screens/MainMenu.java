6ac0386116d4fe431091f1ade934b1efeb94e356
Marika Screpnek <mscrepnek2004@gmail.com>
2025-03-09 12:33:41 -0700

    Add attributes to MainMenu and create load_images method to load button images

--- sfuparkingmayhem/src/main/java/com/sfuparkingmayhem/screens/MainMenu.java
+++ sfuparkingmayhem/src/main/java/com/sfuparkingmayhem/screens/MainMenu.java
@@ -1,2 +1,40 @@
 package com.sfuparkingmayhem.screens;
 
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+public class MainMenu {
+    private int x,y;
+    private BufferedImage start_game;
+    private BufferedImage instructions;
+    private BufferedImage quit;
+
+    public MainMenu(int x, int y) {
+        this.x=x;
+        this.y=y;
+        load_images();
+    }
+
+    private void load_images() {
+        try{
+            this.start_game = ImageIO.read(getClass().getClassLoader().getResourceAsStream("start_game.png"));
+        }
+        catch(IOException e){
+            System.out.println("Error loading main" + e.getMessage());
+        }
+        try{
+            this.instructions = ImageIO.read(getClass().getClassLoader().getResourceAsStream("instructions.png"));
+        }
+        catch(IOException e){
+            System.out.println("Error loading main" + e.getMessage());
+        }
+        try{
+            this.quit = ImageIO.read(getClass().getClassLoader().getResourceAsStream("quit.png"));
+        }
+        catch(IOException e){
+            System.out.println("Error loading main" + e.getMessage());
+        }
+    }
+}
