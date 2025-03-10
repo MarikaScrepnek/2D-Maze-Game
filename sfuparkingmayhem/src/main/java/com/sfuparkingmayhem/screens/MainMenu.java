a0dc8c1a3195f30eff0dc3a2197e238cf31eedff
Marika Screpnek <mscrepnek2004@gmail.com>
2025-03-10 13:58:11 -0700

    load all images

--- sfuparkingmayhem/src/main/java/com/sfuparkingmayhem/screens/MainMenu.java
+++ sfuparkingmayhem/src/main/java/com/sfuparkingmayhem/screens/MainMenu.java
@@ -9,29 +9,69 @@ import javax.swing.JButton;
 import javax.swing.JPanel;
 
 public class MainMenu extends JPanel{
-    private ImageIcon start_game; //start game button image
-    private ImageIcon instructions; //instructions button image
-    private ImageIcon quit; //quit button image
+    private ImageIcon start_game;
+    private ImageIcon start_game_hover;
+    private ImageIcon instructions;
+    private ImageIcon instructions_hover;
+    private ImageIcon exit;
+    private ImageIcon exit_hover;
 
     public MainMenu() { //initializes variables and load button images
         load_images();
+        setPreferredSize(new Dimension(750,750));
         JPanel main_menu = new JPanel();
         setBackground(new Color(111, 194, 232));
         
+        JButton start_game_button = new Jbutton(start_game);
+        JButton instructions_button = new Jbutton(instructions);
+        JButton exit_button = new Jbutton(exit);
+
+        start_game_button.setPreferredSize(new Dimension(192, 64));
+        instructions_button.setPreferredSize(new Dimension(192, 64));
+        exit_button.setPreferredSize(new Dimension(192, 64));
+
     }
 
     private void load_images() { //method to load button images
         ImageIcon start_game = new ImageIcon();
-        ImageIcon instructions = new ImageIcon();
-        ImageIcon quit = new ImageIcon();
-    }
+        try {
+            start_game = ImageIO.read(getClass().getClassLoader().getResourceAsStream("start_game.png"));
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
 
-    public void draw (Graphics g) {
-        JButton start_game_button = new JButton(start_game);
-        start_game_button.setPreferredSize(new Dimension(start_game.getIconWidth(), start_game.getIconHeight()));
-        
-    }
-    public void update() {
-        
+        ImageIcon start_game_hover = new ImageIcon();
+        try {
+            start_game_hover = ImageIO.read(getClass().getClassLoader().getResourceAsStream("start_game_hover.png"));
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        ImageIcon instructions = new ImageIcon();
+        try {
+            instructions = ImageIO.read(getClass().getClassLoader().getResourceAsStream("instructions.png"));
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        ImageIcon instructions_hover = new ImageIcon();
+        try {
+            instructions_hover = ImageIO.read(getClass().getClassLoader().getResourceAsStream("instructions_hover.png"));
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        ImageIcon exit = new ImageIcon();
+        try {
+            quit = ImageIO.read(getClass().getClassLoader().getResourceAsStream("exit.png"));
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        ImageIcon exit = new ImageIcon();
+        try {
+            quit_hover = ImageIO.read(getClass().getClassLoader().getResourceAsStream("exit.png"));
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
