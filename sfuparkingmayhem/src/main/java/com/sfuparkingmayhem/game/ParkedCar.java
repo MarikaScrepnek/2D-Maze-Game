739150474ad169c45e360de261a8706e660a9b50
Ryan Fung <rjf9@sfu.ca>
2025-03-10 12:55:18 -0700

    Finish ParkedCar class

--- sfuparkingmayhem/src/main/java/com/sfuparkingmayhem/game/ParkedCar.java
+++ sfuparkingmayhem/src/main/java/com/sfuparkingmayhem/game/ParkedCar.java
@@ -1,9 +1,25 @@
 package com.sfuparkingmayhem.game;
 
+import java.io.IOException;
+import java.util.Objects;
+
+import javax.imageio.ImageIO;
+
 public class ParkedCar extends Barrier{
 
     ParkedCar(int x, int y){
         super ( x, y);
+        getImage();
+    }
+
+    @Override
+    protected void getImage() {
+        try{
+            this.theImage = ImageIO.read(Objects.requireNonNull(getClass().getClassLoader().getResourceAsStream("car_sprite_down.png")));
+        }
+        catch(IOException e){
+            System.out.println("Error loading main" + e.getMessage());
+        }
     }
 
 }
