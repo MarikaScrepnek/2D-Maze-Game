12101aec0de3df62f5287a8f257888bc5ec8cb56
Marika Screpnek <mscrepnek2004@gmail.com>
2025-03-11 14:51:34 -0700

    Improve code readability in Instructions.java

--- sfuparkingmayhem/src/main/java/com/sfuparkingmayhem/screen/Instructions.java
+++ sfuparkingmayhem/src/main/java/com/sfuparkingmayhem/screen/Instructions.java
@@ -2,6 +2,7 @@ package com.sfuparkingmayhem.screen;
 
 import java.awt.CardLayout;
 import java.awt.Color;
+
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SwingConstants;
@@ -16,37 +17,38 @@ public class Instructions extends Screen {
         instructionsLabel.setFont(kenneyFont);
         instructionsLabel.setForeground(Color.WHITE);
         instructionsLabel.setBounds(200, 50, 350, 50);
-        add(instructionsLabel);
 
         //create the controls title
         JLabel controlsLabel = new JLabel("Controls");
         controlsLabel.setFont(kenneyFont);
         controlsLabel.setForeground(Color.WHITE);
         controlsLabel.setBounds(50, 150, 300, 50);
-        add(controlsLabel);
 
         //create the controls body
         JLabel controlsInfoLabel = new JLabel("<html><brk>Move Up - W<br>Move Down - S<br>Move Left - A<br>Move Right - D</html>");
         controlsInfoLabel.setFont(smallKenneyFont);
         controlsInfoLabel.setForeground(Color.WHITE);
         controlsInfoLabel.setBounds(50, 200, 300, 100);
-        add(controlsInfoLabel);
 
         //create the objective title
         JLabel objectiveLabel = new JLabel("Objective");
         objectiveLabel.setFont(kenneyFont);
         objectiveLabel.setForeground(Color.WHITE);
         objectiveLabel.setBounds(400, 150, 300, 50);
-        add(objectiveLabel);
 
         //create the objective body
         JLabel objectiveInfoLabel = new JLabel("<html><p style='width:250px;'>Collect all 10 coins and escape to the parking booth!<br><br>Avoid hitting parked cars and don't let the parking officer catch you!</p></html>");
         objectiveInfoLabel.setFont(smallKenneyFont);
         objectiveInfoLabel.setForeground(Color.WHITE);
         objectiveInfoLabel.setBounds(400, 200, 300, 200); // Increased width and height
-        add(objectiveInfoLabel);
 
         main_menu_button.setBounds(280, 450, 192, 64);
+
+        add(instructionsLabel);
+        add(controlsLabel);
+        add(controlsInfoLabel);
+        add(objectiveLabel);
+        add(objectiveInfoLabel);
         add(main_menu_button);
     }
 }
