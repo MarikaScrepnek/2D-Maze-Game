3f7655eceee8c8c2262564ebe4d8cf7bd5058e5d
ka-lu <kml43@sfu.ca>
2025-03-11 19:31:07 -0700

    added javadoc to Coin class

--- sfuparkingmayhem/src/main/java/com/sfuparkingmayhem/game/Coin.java
+++ sfuparkingmayhem/src/main/java/com/sfuparkingmayhem/game/Coin.java
@@ -4,6 +4,15 @@ import javax.imageio.ImageIO;
 import java.io.IOException;
 import java.util.Objects;
 
+
+/**
+ * This class extends the Reward class. This coin will have an x and y coordinate to be placed on the board. 
+ * This class has a getImage() to get the png sprite from resources directory and isCollected() to check if this
+ * coin has been collected.
+ * 
+ * @author Group 25
+ * @version 1.0
+ */
 public class Coin extends Reward{
 
     /**
