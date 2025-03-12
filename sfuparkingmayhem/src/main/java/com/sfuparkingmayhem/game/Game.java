1da867ada7912f11172d19602309cdb85ca9583f
Marika Screpnek <mscrepnek2004@gmail.com>
2025-03-11 22:46:07 -0700

    Change game constructor to be the code in initwindow instead of being empty; removed init window call from main

--- sfuparkingmayhem/src/main/java/com/sfuparkingmayhem/game/Game.java
+++ sfuparkingmayhem/src/main/java/com/sfuparkingmayhem/game/Game.java
@@ -20,17 +20,11 @@ import com.sfuparkingmayhem.screen.MainMenu;
  */
 public class Game {
     /**
-     * Constructor for a Game object
-     */
-    public Game() {
-    }
-
-    
-    /**
+     * Constructor for Game object.
      * Initializes the window for the game. Displays different windows to show instructions,
      * main menu, or to indicate if player has won/lost game.
      */
-    public void initWindow()
+    public Game()
     {
         //create a window in which the game will run
         JFrame window = new JFrame("SFU Parking Mayhem");
--- sfuparkingmayhem/src/main/java/com/sfuparkingmayhem/game/Main.java
+++ sfuparkingmayhem/src/main/java/com/sfuparkingmayhem/game/Main.java
@@ -3,6 +3,5 @@ package com.sfuparkingmayhem.game;
 public class Main {
     public static void main( String[] args ) {
         Game game = new Game();
-        game.initWindow();
     }
 }
