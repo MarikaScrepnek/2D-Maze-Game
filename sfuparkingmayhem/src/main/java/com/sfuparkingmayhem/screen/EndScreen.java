132f46df35bee87f573d30b77afef4cfed47648c
Marika Screpnek <mscrepnek2004@gmail.com>
2025-03-11 18:55:21 -0700

    Add JavaDocs to EndScreen.java

--- sfuparkingmayhem/src/main/java/com/sfuparkingmayhem/screen/EndScreen.java
+++ sfuparkingmayhem/src/main/java/com/sfuparkingmayhem/screen/EndScreen.java
@@ -4,7 +4,18 @@ import java.awt.CardLayout;
 
 import javax.swing.JPanel;
 
+/**
+ * Class serves as an abstract base class for game-over screens.
+ * It extends the Screen class and provides a common structure for different
+ * types of end screens, such as win or lose screens.
+ */
 public abstract class EndScreen extends Screen {
+    /**
+     * Constructs an EndScreen with a main menu button which is needed in every end screen.
+     *
+     * @param cardLayout The CardLayout used for switching between screens.
+     * @param cardPanel  The JPanel that contains all the game screens.
+     */
     public EndScreen (CardLayout cardLayout, JPanel cardPanel) {
         //call Screen constructor
         super(cardLayout, cardPanel);
