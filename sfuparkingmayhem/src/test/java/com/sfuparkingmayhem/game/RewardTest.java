3f67b1a00267c84cb8dd18b07f9178626769d4da
ka-lu <kml43@sfu.ca>
2025-03-24 16:16:58 -0700

    added imports to RewardTest

--- sfuparkingmayhem/src/test/java/com/sfuparkingmayhem/game/RewardTest.java
+++ sfuparkingmayhem/src/test/java/com/sfuparkingmayhem/game/RewardTest.java
@@ -1,5 +1,12 @@
 package com.sfuparkingmayhem.game;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import javax.swing.JPanel;
+import java.awt.CardLayout;
+
 public class RewardTest {
     
 }
