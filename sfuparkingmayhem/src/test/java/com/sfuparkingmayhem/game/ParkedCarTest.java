0a025c73696c53dbfdb2ee780f53ea372a317973
ka-lu <kml43@sfu.ca>
2025-03-24 16:10:14 -0700

    added imports to ParkedCarTest

--- sfuparkingmayhem/src/test/java/com/sfuparkingmayhem/game/ParkedCarTest.java
+++ sfuparkingmayhem/src/test/java/com/sfuparkingmayhem/game/ParkedCarTest.java
@@ -1,5 +1,9 @@
 package com.sfuparkingmayhem.game;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class ParkedCarTest {
     
 }
