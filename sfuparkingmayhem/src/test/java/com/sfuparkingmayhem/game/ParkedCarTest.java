c02591e2e35c866779fcd79df1ecb1bbe24750a1
ka-lu <kml43@sfu.ca>
2025-03-24 16:12:59 -0700

    added setUp() to ParkedCarTest

--- sfuparkingmayhem/src/test/java/com/sfuparkingmayhem/game/ParkedCarTest.java
+++ sfuparkingmayhem/src/test/java/com/sfuparkingmayhem/game/ParkedCarTest.java
@@ -4,6 +4,26 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+import javax.swing.JPanel;
+import java.awt.CardLayout;
+
 public class ParkedCarTest {
+
+    ParkedCar pc;
+    Board aBoard;
+    CardLayout cardLayout;
+    JPanel cardPanel;
+
+    @BeforeEach
+    public void setUp(){
+
+        cardLayout = new CardLayout();
+        cardPanel = new JPanel(cardLayout);
+        aBoard = new Board(cardLayout, cardPanel);
+
+        pc = new ParkedCar(0, 0, aBoard);
+        
+    }
+
     
 }
