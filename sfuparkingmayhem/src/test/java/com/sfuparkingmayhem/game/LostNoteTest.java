8b41c61a1c8bd569a0accc8945b78087c680379b
Marika Screpnek <mscrepnek2004@gmail.com>
2025-03-25 12:27:41 -0700

    Create setup test for LostNote

--- sfuparkingmayhem/src/test/java/com/sfuparkingmayhem/game/LostNoteTest.java
+++ sfuparkingmayhem/src/test/java/com/sfuparkingmayhem/game/LostNoteTest.java
@@ -1,5 +1,23 @@
 package com.sfuparkingmayhem.game;
 
+import java.awt.CardLayout;
+
+import javax.swing.JPanel;
+
+import org.junit.jupiter.api.BeforeEach;
+
 public class LostNoteTest {
-    
+    CardLayout cardLayout;
+    JPanel cardPanel;
+    Board board;
+    LostNote lostNote;
+
+    @BeforeEach
+    public void setUp(){
+        cardLayout = new CardLayout();
+        cardPanel = new JPanel(cardLayout);
+        board = new Board(cardLayout, cardPanel);
+
+        lostNote = new LostNote(0, 0, board);
+    }
 }
