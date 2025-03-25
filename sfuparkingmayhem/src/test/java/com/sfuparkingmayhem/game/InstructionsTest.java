76bd6217394a2797ba44ce192eacda75d85ab536
Marika Screpnek <mscrepnek2004@gmail.com>
2025-03-25 12:55:59 -0700

    Create instructions screen test

--- sfuparkingmayhem/src/test/java/com/sfuparkingmayhem/game/InstructionsTest.java
+++ sfuparkingmayhem/src/test/java/com/sfuparkingmayhem/game/InstructionsTest.java
@@ -1,5 +1,31 @@
 package com.sfuparkingmayhem.game;
 
+import java.awt.CardLayout;
+
+import javax.swing.JPanel;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.sfuparkingmayhem.screen.Instructions;
+
 public class InstructionsTest {
-    
+    CardLayout cardLayout;
+    JPanel cardPanel;
+
+    Instructions instructions;
+
+    @BeforeEach
+    public void setup() {
+        cardLayout = new CardLayout();
+        cardPanel = new JPanel();
+
+        instructions = new Instructions(cardLayout, cardPanel);
+    }
+
+    @Test
+    public void testInstructionsConstruction() {
+        assertNotNull(instructions, "Instructions should not be null after setup.");
+    }
 }
