9f46fd1de1b6c99fc58f0146eb5d8a1dabf97c05
Marika Screpnek <mscrepnek2004@gmail.com>
2025-03-25 13:29:53 -0700

    Add constructor test in losescreenconcord

--- sfuparkingmayhem/src/test/java/com/sfuparkingmayhem/game/LoseScreenConcordTest.java
+++ sfuparkingmayhem/src/test/java/com/sfuparkingmayhem/game/LoseScreenConcordTest.java
@@ -1,5 +1,31 @@
 package com.sfuparkingmayhem.game;
 
+import java.awt.CardLayout;
+
+import javax.swing.JPanel;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.sfuparkingmayhem.screen.LoseScreenConcord;
+
 public class LoseScreenConcordTest {
-    
+    LoseScreenConcord loseScreenConcord;
+
+    CardLayout cardLayout;
+    JPanel cardPanel;
+
+    @BeforeEach
+    public void setup() {
+        cardLayout = new CardLayout();
+        cardPanel = new JPanel();
+
+        loseScreenConcord = new LoseScreenConcord(cardLayout, cardPanel);
+    }
+
+    @Test
+    public void testLoseScreenConcordConstruction() {
+        assertNotNull(loseScreenConcord, "LoseScreenConcord should not be null after setup.");
+    }
 }
