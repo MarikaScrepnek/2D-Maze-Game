b3a25d489a9d804b7d6b2d1f2267f539d6a6e3ac
Marika Screpnek <mscrepnek2004@gmail.com>
2025-03-25 13:34:26 -0700

    Add constructor test in mainmenutest

--- sfuparkingmayhem/src/test/java/com/sfuparkingmayhem/game/MainMenuTest.java
+++ sfuparkingmayhem/src/test/java/com/sfuparkingmayhem/game/MainMenuTest.java
@@ -1,5 +1,32 @@
 package com.sfuparkingmayhem.game;
 
+import java.awt.CardLayout;
+
+import javax.swing.JPanel;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.sfuparkingmayhem.screen.MainMenu;
+
 public class MainMenuTest {
-    
+    MainMenu mainMenu;
+
+    CardLayout cardLayout;
+    JPanel cardPanel;
+
+    @BeforeEach
+    public void setup() {
+        cardLayout = new CardLayout();
+        cardPanel = new JPanel();
+
+
+        mainMenu = new MainMenu(cardLayout, cardPanel);
+    }
+
+    @Test
+    public void testMainMenuConstruction() {
+        assertNotNull(mainMenu, "MainMenu should not be null after setup.");
+    }
 }
