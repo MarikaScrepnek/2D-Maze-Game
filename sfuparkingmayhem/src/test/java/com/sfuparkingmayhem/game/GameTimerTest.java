d9217ead780aad50ac51c0a211bbab5bcf99cf42
Matthew Pham <matthew_pham@sfu.ca>
2025-03-25 00:41:11 -0700

    Add test for getTimeElapsed and draw methods

--- sfuparkingmayhem/src/test/java/com/sfuparkingmayhem/game/GameTimerTest.java
+++ sfuparkingmayhem/src/test/java/com/sfuparkingmayhem/game/GameTimerTest.java
@@ -1,5 +1,42 @@
 package com.sfuparkingmayhem.game;
 
-public class GameTimerTest {
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import javax.swing.Timer;
+
+class GameTimerTest {
+    private int timeElapsed;
+    private GameTimer gameTimer;
+    private Board board;
+
+    @BeforeEach
+    void setUp() {
+        //board = new Board();
+        gameTimer = new GameTimer(board);
+    }
+
+    @Test
+    void testGetTimeElapsed() {
+        //timeElapsed = 10;
+        assertEquals(timeElapsed, gameTimer.getTimeElapsed());
+    }
+
+    @Test 
+    void testDraw() {
+        Graphics g = mock(Graphics.class);
+        gameTimer.draw(g);
+        verify(g).setColor(Color.WHITE);
+        verify(g).setFont(new Font("Bahnschrift", Font.BOLD, 25));
+        verify(g).drawString("00:00", 343, 30);
+    }
+
+    // Test lines 14 and 15 in GameTimer.java 
     
+    
+
 }
