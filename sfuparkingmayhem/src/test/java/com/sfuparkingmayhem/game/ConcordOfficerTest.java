99067a3d9a866b180e103b96b435dfeb17255ee9
ka-lu <kml43@sfu.ca>
2025-03-24 19:34:53 -0700

    added setUp() to concordofficertest

--- sfuparkingmayhem/src/test/java/com/sfuparkingmayhem/game/ConcordOfficerTest.java
+++ sfuparkingmayhem/src/test/java/com/sfuparkingmayhem/game/ConcordOfficerTest.java
@@ -7,5 +7,23 @@ import static org.junit.jupiter.api.Assertions.*;
 import javax.swing.JPanel;
 import java.awt.CardLayout;
 public class ConcordOfficerTest {
+
+
+    ConcordOfficer officer;
+    Board aBoard;
+    CardLayout cardLayout;
+    JPanel cardPanel;
+
+    @BeforeEach
+    public void setUp(){
+
+        cardLayout = new CardLayout();
+        cardPanel = new JPanel(cardLayout);
+        aBoard = new Board(cardLayout, cardPanel);
+
+        officer = new ConcordOfficer(1,2,aBoard);
+        
+    }
+
     
 }
