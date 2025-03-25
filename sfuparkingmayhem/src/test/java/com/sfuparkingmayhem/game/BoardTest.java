c1766b51376f271e35dd11f41495638b3fe10999
ka-lu <kml43@sfu.ca>
2025-03-24 19:02:38 -0700

    added setUp() to Boardtest

--- sfuparkingmayhem/src/test/java/com/sfuparkingmayhem/game/BoardTest.java
+++ sfuparkingmayhem/src/test/java/com/sfuparkingmayhem/game/BoardTest.java
@@ -1,5 +1,32 @@
 package com.sfuparkingmayhem.game;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import javax.swing.JPanel;
+import java.awt.CardLayout;
+
+
+import javax.swing.JPanel;
+
 public class BoardTest {
+
+    Board board;
+    Board aBoard;
+    CardLayout cardLayout;
+    JPanel cardPanel;
+
+    @BeforeEach
+    public void setUp(){
+
+        cardLayout = new CardLayout();
+        cardPanel = new JPanel(cardLayout);
+        aBoard = new Board(cardLayout, cardPanel);
+
+        board = new Board(cardLayout, cardPanel);
+        
+    }
+    
     
 }
