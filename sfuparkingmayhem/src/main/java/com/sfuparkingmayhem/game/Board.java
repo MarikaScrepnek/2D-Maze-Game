f2fd81dd4c0df0f26a36b646ac67820e62a80f3e
ka-lu <kml43@sfu.ca>
2025-03-27 12:50:30 -0700

    removed the e arraylists containing the entities' coordinates

--- sfuparkingmayhem/src/main/java/com/sfuparkingmayhem/game/Board.java
+++ sfuparkingmayhem/src/main/java/com/sfuparkingmayhem/game/Board.java
@@ -108,21 +108,6 @@ public class Board extends JPanel implements ActionListener, KeyListener{
     JPanel cardPanel;
 
     /**
-     * Array holding the coordinates for coins.
-     */
-    private int[][] coin_coords = {{2, 7, 10, 7, 3, 11, 6, 12, 9, 3}, {3, 2, 4, 5, 8, 8, 9, 11, 12, 13}};
-
-    /**
-     * Array holding the coordinates for cones.
-     */
-    private int[][] cone_coords = {{4, 11, 12, 4, 9, 6, 12, 2, 10, 7}, {3, 3, 3, 5, 7, 8,9, 10, 10, 13}};
-
-    /**
-     * Array holding the coordinates for parked cars.
-     */
-    private int[][] pc_coords = {{2, 6, 8, 12, 12, 3, 4, 4, 8, 9}, {2, 5, 5, 6, 7, 7, 11, 12, 11, 11}};
-
-    /**
      * All of the coordinates on the board for each entity
      */
     private EntityCoordinates EntCoords = new EntityCoordinates();
