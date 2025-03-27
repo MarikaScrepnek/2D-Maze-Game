09a9318b9be71eaee68de6c4547c8fa889e54c57
Marika Screpnek <mscrepnek2004@gmail.com>
2025-03-27 12:39:33 -0700

    Implement EntityLists.java

--- sfuparkingmayhem/src/main/java/com/sfuparkingmayhem/game/EntityLists.java
+++ sfuparkingmayhem/src/main/java/com/sfuparkingmayhem/game/EntityLists.java
@@ -1,5 +1,34 @@
 package com.sfuparkingmayhem.game;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class EntityLists {
-    
+    /**
+     * Arraylists holding all coins in the game.
+     */
+    ArrayList<Coin> coins;
+    /**
+     * Arraylists holding all cones in the game.
+     */
+    ArrayList<Cone> cones;
+    /**
+     * Arraylists holding all parked cars in the game.
+     */
+    ArrayList<ParkedCar> parkedCars;
+    /**
+     * Arraylists holding all lost notes in the game.
+     */
+    List<LostNote> ln;
+
+    /**
+     * Initializes ArrayLists to hold all types of Entities
+     * (coins, cones, parked cars, lost notes).
+     */
+    public EntityLists() {
+        coins = new ArrayList<>();
+        cones = new ArrayList<>();
+        parkedCars = new ArrayList<>();
+        ln = new ArrayList<>(); 
+    }
 }
