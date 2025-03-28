8c08e717b62aca1259ac68f3675610634ead9df3
Matthew Pham <matthew_pham@sfu.ca>
2025-03-28 14:27:56 -0700

    Add test for keyevent handlers

--- sfuparkingmayhem/src/test/java/com/sfuparkingmayhem/game/BoardTest.java
+++ sfuparkingmayhem/src/test/java/com/sfuparkingmayhem/game/BoardTest.java
@@ -4,12 +4,16 @@ import java.awt.CardLayout;
 
 import javax.swing.JPanel;
 
+import static org.mockito.Mockito.*;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import java.awt.event.KeyEvent;
+
 
 public class BoardTest {
 
@@ -18,17 +22,21 @@ public class BoardTest {
     JPanel cardPanel;
     LostNote ln;
     EntityLists entityLists;
+    MainCharacter mockMainCharacter;
 
     @BeforeEach
     public void setUp(){
 
         cardLayout = new CardLayout();
         cardPanel = new JPanel(cardLayout);
+        mockMainCharacter = mock(MainCharacter.class);
 
         board = new Board(cardLayout, cardPanel);
         ln = new LostNote(0, 0, board);
 
         entityLists = new EntityLists();
+
+        board.main_character = mockMainCharacter;
     }
 
     @Test
@@ -63,5 +71,28 @@ public class BoardTest {
         board.entityLists.ln.remove(0);
         assertTrue(board.entityLists.ln.isEmpty());
         assertEquals(0, board.entityLists.ln.size());
+    }
+
+    @Test
+    void testKeyTyped() {
+        board.keyTyped(null);
+    }
+
+    @Test
+    void testKeyPressed() {
+        KeyEvent mockKeyEvent = mock(KeyEvent.class);
+
+        board.keyPressed(mockKeyEvent);
+
+        verify(mockMainCharacter).delayedMove(mockKeyEvent);
+    }
+
+    @Test 
+    void testKeyReleased() {
+        KeyEvent mockKeyEvent = mock(KeyEvent.class);
+
+        board.keyReleased(mockKeyEvent);
+        verify(mockMainCharacter).keyReleased(mockKeyEvent);
+
     }
 }
