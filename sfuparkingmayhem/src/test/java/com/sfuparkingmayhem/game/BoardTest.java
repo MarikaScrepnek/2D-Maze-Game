b874ffa086aff9dc38b267fe80510eef9e1384aa
Matthew Pham <matthew_pham@sfu.ca>
2025-03-27 17:44:20 -0700

    Add test for timer functionality

--- sfuparkingmayhem/src/test/java/com/sfuparkingmayhem/game/BoardTest.java
+++ sfuparkingmayhem/src/test/java/com/sfuparkingmayhem/game/BoardTest.java
@@ -4,6 +4,7 @@ import java.awt.CardLayout;
 
 import javax.swing.JPanel;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -13,6 +14,8 @@ public class BoardTest {
     Board board;
     CardLayout cardLayout;
     JPanel cardPanel;
+    LostNote ln;
+    EntityLists entityLists;
 
     @BeforeEach
     public void setUp(){
@@ -21,11 +24,23 @@ public class BoardTest {
         cardPanel = new JPanel(cardLayout);
 
         board = new Board(cardLayout, cardPanel);
+        ln = new LostNote(0, 0, board);
+
+        entityLists = new EntityLists();
     }
 
     @Test
     public void testBoardConstruction() {
         assertNotNull(board, "Board should not be null after setup.");
+    }
+
+    @Test 
+    void testLostNoteTimer() throws InterruptedException {
+        int initialSize = board.entityLists.ln.size();
+
+        Thread.sleep(7000);
+
+        assertEquals(initialSize, board.entityLists.ln.size()); 
     }
     
     
